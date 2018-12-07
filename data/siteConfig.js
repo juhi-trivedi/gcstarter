3769ee6f5e28d723aed7145388c33c7e31838435
Juhi Trivedi <juhi.trivedi@multidots.com>
2018-12-07 13:00:56 +0530

    Added disqus

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,14 +1,7 @@
 module.exports = {
-  siteTitle: 'gatsby-contentful',
-  siteDescription: "👋 Hey I'm a Gatsby starter!",
+  siteTitle: 'gatsby-contentful-demo',
+  siteDescription: "👋 Hey There! I'm a GatsbyJs starter!",
   authorName: 'J T',
-  authorDescription: `
-  For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
-  On his day to day job, he is working as a senior front-end engineer at VSware. He is also a frequent tech speaker and a mentor.
-  As a new digital nomad, he is living where the WIFI and sun is 😎 <br>
-  Do you want to know more?
-  `,
   disqusSiteUrl: 'https://gcstarter.netlify.com',
-  // Prefixes all links. For cases when deployed to maxpou.fr/gatsby-starter-morning-dew/
   disqusShortname: 'gcstarter',
 }
