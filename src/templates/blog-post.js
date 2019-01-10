fbb0450188202568cee270b5d53c05816b27a297
Juhi Trivedi <juhi.trivedi@multidots.com>
2019-01-10 11:49:27 +0530

    Added auth in blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,29 +1,74 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Disqus from '../components/Disqus'
+import withAuthorization from '../components/Session/withAuthorization'
 import { connect } from 'react-redux'
+import { compose } from 'recompose'
+import cookie from 'react-cookies'
+import { navigate } from 'gatsby'
 // import { compose } from 'recompose'
+
+const BlogPageContent = ({ data }) => (
+<>
+  <Helmet title={`${data.title}`} />
+  <div className="container blogdetails">
+    <h1>{data.title}</h1>
+    <div
+      dangerouslySetInnerHTML={{ __html: data.body.childMarkdownRemark.html }}
+    />
+    <Disqus slug={data.slug} title={data.title} />
+  </div>
+</>
+)
+
+const authCondition = authUser => !!authUser
+const mapStateToProps = state => {
+  return {
+    users: state,
+  }
+}
+
+const BlogPagePost = compose(
+  connect(
+    mapStateToProps,
+    null
+  ),
+  withAuthorization(authCondition)
+)(BlogPageContent)
+
 class BlogPost extends Component {
   handleNewComment(comment) {
     console.log(comment.text)
   }
-
+  constructor() {
+    super()
+    this.state = {
+      route: '',
+    }
+  }
+  componentDidMount() {
+    //this.setState({ route: navigate('/') })
+    const saveData = cookie.load('authUser')
+    if (saveData) {
+      this.setState({ route: '' })
+    } else {
+      this.setState({ route: navigate('/') })
+    }
+  }
   render() {
-    const { title, body, slug } = this.props.data.contentfulBlog
+    const saveData = cookie.load('authUser')
     return (
+      <Fragment>
+        {saveData ? (
       <Layout>
-        <Helmet title={`${title}`} />
-        <div className="container blogdetails">
-          <h1>{title}</h1>
-          <div
-            dangerouslySetInnerHTML={{ __html: body.childMarkdownRemark.html }}
-          />
-          <Disqus slug={slug} title={title} />
-        </div>
-        {/* <PrevNextPost previous={previous} next={next} /> */}
+       <BlogPagePost data={this.props.data.contentfulBlog} />
       </Layout>
+        ) : (
+          this.state.route
+        )}
+      </Fragment>
     )
   }
 }
@@ -32,16 +77,9 @@ BlogPost.propTypes = {
   data: PropTypes.object.isRequired,
 }
 
-const mapStateToProps = state => {
-  return {
-    users: state,
-  }
-}
 
-export default connect(
-  mapStateToProps,
-  null
-)(BlogPost)
+
+export default BlogPost
 
 export const pageQuery = graphql`
   query blogPostQuery($slug: String!) {
